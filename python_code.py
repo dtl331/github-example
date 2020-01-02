b5fa9ad1702bf92275e49da606b850e75ef6a720
dtl331 <59376831+dtl331@users.noreply.github.com>
2020-01-02 18:46:04 -0500

    Update python_code.py

--- python_code.py
+++ python_code.py
@@ -1,5 +1,4 @@
 ### This notebook will be mainly used for the capstone project
-<h1 align="center"><font size="5">Classification with Python</font></h1>
 import pandas as pd
 import numpy as np
 print("Hello Capstone Project Course!")
